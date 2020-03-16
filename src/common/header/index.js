165f1b00d46a1cd50432143d2ac0d2d89a435bf2
xingyifan <xingyifan10@qq.com>
2020-03-16 21:26:17 +0800

    热门搜索样式布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
+
 // import * as actionCreators from "./store/actionCreators";
 import { actionCreators } from "./store";
+
 import {
   HeaderWrapper,
   Logo,
@@ -10,45 +12,81 @@ import {
   NavItem,
   Addition,
   Button,
+  SearchInfo,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoList,
+  SearchInfoItem,
   SearchWrapper,
   NavSearch
 } from "./style";
 
-// 此时Header是一个无状态组件，改写成函数，无状态组件性能高
+class Header extends Component {
+  getListArea(show) {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>hahah</SearchInfoItem>
+            <SearchInfoItem>hahah</SearchInfoItem>
+            <SearchInfoItem>hahah</SearchInfoItem>
+            <SearchInfoItem>hahah</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      );
+    } else {
+      return null;
+    }
+  }
 
-const Header = props => {
-  return (
-    <HeaderWrapper>
-      <Logo href="./" />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载APP</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">
-          <i className="iconfont">&#xe636;</i>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition in={props.focused} timeout={200} classNames="slide">
-            <NavSearch
-              className={props.focused ? "focused" : ""}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <i className={props.focused ? "focused iconfont" : "iconfont"}>
-            &#xe614;
-          </i>
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className="writting">
-          <i className="iconfont">&#xe615;</i>写文章
-        </Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo href="./" />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载APP</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={this.props.focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.props.focused ? "focused" : ""}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={this.props.focused ? "focused iconfont" : "iconfont"}>
+              &#xe614;
+            </i>
+            {this.getListArea(this.props.focused)}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writting">
+            <i className="iconfont">&#xe615;</i>写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    );
+  }
+}
+
+// 此时Header是一个无状态组件，改写成函数，无状态组件性能高
+// const Header = props => {
+//   return (props.focused);
+// };
 
 const mapStateToProps = state => {
   return {
