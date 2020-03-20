e47a63ed2e288a2f6e06e1469918d71e2a27e40e
xingyifan <xingyifan10@qq.com>
2020-03-20 21:31:04 +0800

    解构赋值，switchcase优化reducer

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -12,16 +12,30 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
   console.log("接收参数action::", action);
-  if (action.type === constants.SEARCH_FOCUS) {
-    // immutable 的 set 方法会结合之前 immutable 对象的值，和设置的值，返回一个全新的不可变对象
-    return state.set("focused", true);
+
+  switch (action.type) {
+    case constants.SEARCH_FOCUS:
+      return state.set("focused", true);
+    case constants.SEARCH_BLUR:
+      return state.set("focused", false);
+    case constants.CHANGE_LIST:
+      return state.set("list", action.data);
+    default:
+      return state;
   }
-  if (action.type === constants.SEARCH_BLUR) {
-    return state.set("focused", false);
-    // return { focused: false };
-  }
-  if (action.type === constants.CHANGE_LIST) {
-    return state.set("list", action.data);
-  }
-  return state;
+
+  // 每条case没有break是因为已经return了
+
+  // if (action.type === constants.SEARCH_FOCUS) {
+  //   // immutable 的 set 方法会结合之前 immutable 对象的值，和设置的值，返回一个全新的不可变对象
+  //   return state.set("focused", true);
+  // }
+  // if (action.type === constants.SEARCH_BLUR) {
+  //   return state.set("focused", false);
+  //   // return { focused: false };
+  // }
+  // if (action.type === constants.CHANGE_LIST) {
+  //   return state.set("list", action.data);
+  // }
+  // return state;
 };
